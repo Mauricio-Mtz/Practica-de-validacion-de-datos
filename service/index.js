3337ec8e2df15c8a71331c9cbabd56922e2ef42f
Mauricio <136762122+Mauricio-Mtz@users.noreply.github.com>
2025-01-28 22:50:44 -0600

    Practica de validación de backend API POST

--- service/index.js
+++ service/index.js
@@ -3,37 +3,76 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 const port = 3000
+
+let usuarios = [
+  {
+    usuario: 'McPeMauri',
+    correo: 'mcpemauri@gmail.com',
+    contrasena: '1224',
+    cumpleanios: '24/09/2003',
+    nombre: 'Mauricio Martínez',
+  },
+]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/usuarios', (req, res) => {
-  const usuarioOriginal = 'Mauricio'
-  const correoOriginal = 'mauri@gmail.com'
-  const contrasenaOriginal = 'Mau1224!"'
-  const telefonoOriginal = '4411108592'
 
-  const usuario = req.headers.usuario
-  const contrasena = req.headers.contrasena
+app.get('/login', (req, res) => {
+  const usuarioRecibido = req.headers.usuario
+  const contrasenaRecibida = req.headers.contrasena
 
-  if (usuario === usuarioOriginal && contrasena === contrasenaOriginal) {
+  const usuario = usuarios.find(
+    (u) => u.usuario === usuarioRecibido && u.contrasena === contrasenaRecibida
+  )
+  if (usuario) {
     res.json({
       statusCode: 200,
-      intMessage: 'Operation Successfull',
+      intMessage: 'Operation Successful',
       data: {
-        usuario: usuarioOriginal,
-        correo: correoOriginal,
-        contrasena: telefonoOriginal,
-        telefono: telefonoOriginal,
+        usuario: usuario.usuario,
+        correo: usuario.correo,
+        cumpleanios: usuario.cumpleanios,
+        nombre: usuario.nombre,
       },
     })
   } else {
     res.json({
-        statusCode: 401,
-        intMessage: 'Operation Failed',
-        data: {},
+      statusCode: 401,
+      intMessage: 'Operation Failed',
+      data: {},
+    })
+  }
+})
+
+app.post('/register', (req, res) => {
+  const { usuario, correo, contrasena, cumpleanios, nombre } = req.body
+  if (!usuario || !correo || !contrasena || !cumpleanios || !nombre) {
+    res.json({
+      statusCode: 400,
+      intMessage: 'Missing fields'
+    })
+    return
+  } else if (usuarios.find((u) => u.usuario === usuario || u.correo === correo)) {
+    res.json({
+      statusCode: 400,
+      intMessage: 'Credentials already in use'
+    })
+    return
+  } else {
+    usuarios.push({ usuario, correo, contrasena, cumpleanios, nombre })
+    res.json({
+      statusCode: 200,
+      intMessage: 'Operation Successful',
+      data: {
+        usuario,
+        correo,
+        cumpleanios,
+        nombre,
+      },
     })
   }
 })
